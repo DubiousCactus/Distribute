044e39b8aef4b97479733ebea893fdd21e6d552f
Mikkel Bayard Rasmussen <mbrbayard@live.dk>
2018-11-04 16:02:29 +0100

    REST server works, POST and GET calls can be accessed though a website now.

--- lead/flaskServer.py
+++ lead/flaskServer.py
@@ -5,14 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def showpage():
    return render_template('upload.html')
-	
-@app.route('/upload', methods = ['GET', 'POST'])
+  
+@app.route('/storage', methods = ['GET','POST'])
 def upload_file():
+
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename)) #WILL BE REMOVED BECAUSE WE DONT STORE ON THE PI3
 	  #REMOTE CALL TO RANDOM NODE IN THE SYSTEM USING RPC
       return 'file uploaded successfully'
+      
+   if request.method == 'GET':
+         return 'returned '+ request.args["filename"]+' successfully'
+         #return 'returning '+ fileEntered +' successfully'
 		
 if __name__ == '__main__':
 	app.run(debug=True, host="localhost", port=5000)
