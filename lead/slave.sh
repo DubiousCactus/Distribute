6023f5886b79f571b0fc608fb35bf81b476e9574
Theo Morales <tml@trifork.com>
2018-11-13 15:59:26 +0100

    Update deploy script

---
+++ lead/slave.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# slave.sh
+# Copyright (C) 2018 theomorales <theomorales@Theos-MBP>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+/usr/bin/python3 client_node.py
--- slave/client_node.py
+++ slave/client_node.py
@@ -1,21 +1,23 @@
 #! /usr/bin/python3
+
 import json
 import requests
 import netifaces as ni
+
 from uuid import getnode as get_mac
-import codeversion from lead.codeversion
 
 class ClientNode:
-    leadIP = "172.24.1.1"
-    leadReqestPort = "5001"
-    leadCommunicationPort = "5002"
 
-    def __init__(self, port):
+    def __init__(self, config):
+        self.leadIP = config['lead_ip']
+        self.leadPort = config['lead_port']
+        self.port = config['port']
+        self._version = config['version']
         self.register()
 
     def start(self):
         # Starts the communication service as a server
-        run_simple(get_ip(),leadCommunicationPort,application)
+        run_simple(get_ip(), self.leadPort, application)
 
     def read_file(self, filepath):
         file = open(filepath, "rb").read()
@@ -23,22 +25,19 @@ class ClientNode:
 
     def make_payload(self, method, params):
         return {
-            "method":method,
-            "params":params,
-            "jsonrpc":"2.0",
-            "id":0,
+            "method": method,
+            "params": params,
+            "jsonrpc": "2.0",
+            "id": 0,
         }
 
     def get_ip(self):
         return ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 
     def remote_call(self, payload):
-        url = "http://"+self.leadIP+":"+self.leadReqestPort
-        headers = {'content-type':'application/json'}
-        return requests.post(url, data = json.dumps(payload), headers=headers).json()
-
-    def getCodeVersion():
-        pass
+        url = "http://{}:{}".format(self.leadIP, self.leadReqestPort)
+        headers = {'content-type': 'application/json'}
+        return requests.post(url, data=json.dumps(payload), headers=headers).json()
 
     def register(self):
         payload = make_payload(
@@ -46,16 +45,12 @@ class ClientNode:
             {
                 "ip": get_ip(),
                 "mac": hex(get_mac()),
-                "code": getCodeVersion()
+                "code": self._version
             }
         )
         response = remote_call(payload)
 
-        #assert response["result"] == 3
-        # assert response["jsonrpc"]
-        # assert response["id"] == 0
-
-#SERVER PART START
+    #SERVER PART START
     @dispatcher.add_method
     def write_file(**kwargs):
         TO = os.getcwd()+"\\"+kwargs["filename"]
@@ -80,9 +75,10 @@ class ClientNode:
         response = JSONRPCResponseManager.handle(request.data, dispatcher)
         print(request.data)
         return Response(response.json,mimetype='application/json')
-#SERVER PART END
+    #SERVER PART END
+
 
 if __name__ == "__main__":
-    client = ClientNode(5000)
-    register()
-    client.start()
+    with open('config.json') as config_file:
+        client = ClientNode(json.load(config_file))
+        client.start()
